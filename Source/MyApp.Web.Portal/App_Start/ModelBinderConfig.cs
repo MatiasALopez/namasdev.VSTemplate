9107d138ff0c4a2f91b4c433e4fd637a815ba58e
Matias Lopez <matutelopez@hotmail.com>
2025-06-05 11:16:25 -0300

    [main] (Web.Portal) Actualizar: model binder

--- Source/MyApp.Web.Portal/App_Start/ModelBinderConfig.cs
+++ Source/MyApp.Web.Portal/App_Start/ModelBinderConfig.cs
@@ -10,8 +10,16 @@ namespace MyApp.Web.Portal
         {
             System.Web.Mvc.ModelBinders.Binders.Add(typeof(DateTime), new DateTimeModelBinder());
             System.Web.Mvc.ModelBinders.Binders.Add(typeof(DateTime?), new DateTimeModelBinder());
+            System.Web.Mvc.ModelBinders.Binders.Add(typeof(int), new IntegerModelBinder());
+            System.Web.Mvc.ModelBinders.Binders.Add(typeof(int?), new IntegerModelBinder());
+            System.Web.Mvc.ModelBinders.Binders.Add(typeof(short), new ShortModelBinder());
+            System.Web.Mvc.ModelBinders.Binders.Add(typeof(short?), new ShortModelBinder());
+            System.Web.Mvc.ModelBinders.Binders.Add(typeof(long), new LongModelBinder());
+            System.Web.Mvc.ModelBinders.Binders.Add(typeof(long?), new LongModelBinder());
             System.Web.Mvc.ModelBinders.Binders.Add(typeof(decimal), new DecimalModelBinder());
             System.Web.Mvc.ModelBinders.Binders.Add(typeof(decimal?), new DecimalModelBinder());
+            System.Web.Mvc.ModelBinders.Binders.Add(typeof(double), new DoubleModelBinder());
+            System.Web.Mvc.ModelBinders.Binders.Add(typeof(double?), new DoubleModelBinder());
         }
     }
 }
